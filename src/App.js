c9e4dba96f58ba08b7ca0471ebc972452c4875fe
ferenc_koler <ferenckoler@outlook.com>
2023-09-14 16:19:25 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
+import Pagination from './components/Pagination/Pagination';
+import Search from './components/Search/Search';
+
+function App() {
+
+  let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState('');
+
+  // console.log(pageNumber);
+  let [fetchedData, updateFetchedData] = useState([]);
+
+  let { info, results } = fetchedData;
+
+  // console.log(results);
+  
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+
+  useEffect(()=>{
+
+    (async function(){
+
+      let data = await fetch(api).then(res => res.json());
+
+      // console.log(data.results);
+      updateFetchedData(data);      
+    })();
+  }, [api]);
+
+
+  return (
+    <div className="App">
+
+      <h1 className="text-center rocksalt my-5">
+        Rick & Morty <span className="text-primary">Characters</span> 
+      </h1>
+
+      <Search setPageNumber={setPageNumber} setSearch={setSearch} />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <div className="row">
+                <Cards results={results}/>                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Pagination 
+        info={info} 
+        pageNumber={pageNumber} 
+        setPageNumber={setPageNumber} 
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+
+const Pagination = ({ info, pageNumber, setPageNumber }) => {  
+  // console.log(info.pages);
+  // console.log(pageNumber);
+  return <ReactPaginate
+    containerClassName="pagination" 
+    className="pagination justify-content-center gap-4 my-4"
+    forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+    breakLabel=". . ."
+    nextLabel="Next"
+    previousLabel="Prev"
+    nextClassName="btn"
+    previousClassName="btn"
+    pageClassName="page-item"
+    pageLinkClassName="page-link"
+    activeClassName="active"
+    onPageChange={(data) => {
+      setPageNumber(data.selected + 1);
+    }}
+    pageCount={info?.pages}
+  />;
+};
+
+//   return <div className='container d-flex justify-content-center gap-5 my-5'>
+//     <button onClick={prev} className="btn btn-primary">Prev</button>
+//     <button onClick={next} className="btn btn-primary">Next</button>
+//   </div>;
+// };
+
+export default Pagination;
